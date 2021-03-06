4de1b789e9792561cc2357e3bc96637141b9a406
camposCode <info@josecampos.io>
2021-03-06 14:41:47 +0100

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,52 @@
 import React, { useEffect } from 'react';
-import{ v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
 
-    
-    const updateTodo = (title, id, completed) =>{
-        const newTodo = todos.map((todo) => 
+
+    const updateTodo = (title, id, completed) => {
+        const newTodo = todos.map((todo) =>
             todo.id === id ? { title, id, completed } : todo
         );
         setTodos(newTodo);
-        setEditTodo( " ");
+        setEditTodo(" ");
     };
 
     useEffect(() => {
-        if(editTodo){
+        if (editTodo) {
             setInput(editTodo.title);
-        }else{
+        } else {
             setInput(" ")
         }
-    }, [ setInput, editTodo]);
+    }, [setInput, editTodo]);
 
-    const onInputChange = (e) =>{
+    const onInputChange = (e) => {
         setInput(e.target.value);
     };
 
-    const onFormSubmit = (e) =>{
+    const onFormSubmit = (e) => {
         e.preventDefault();
-        if(!editTodo){
-            setTodos([...todos, { id: uuidv4(), title: input, completed: false}]);
+        if (!editTodo) {
+            setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
             setInput(" ");
-        }else{
+        } else {
             updateTodo(input, editTodo.id, editTodo.completed)
         }
-        
+
     };
 
-    return (
-        <form onSubmit = { onFormSubmit }>
-            <input 
-                type="text" 
-                placeholder ="Enter a Todo..." 
-                className = "task-input"
-               value= { input }
-                required
-                onChange = { onInputChange }
-            />
-            <button className = "button-add" type = "submit">{ editTodo ? "OK" :  "Add"}</button>
-        </form>
+    return ( 
+        <form onSubmit = { onFormSubmit } >
+           
+            
+            <label className = "label">Your task:</label>
+            < input type = "text"
+            placeholder = "Enter a Todo..."
+            className = "task-input"
+            value = { input }
+            required onChange = { onInputChange }/> 
+            <button className = "button-add"
+            type = "submit" > { editTodo ? "OK" : "Add" } < /button> 
+        < /form >
     )
 }
